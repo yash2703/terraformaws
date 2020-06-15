685ba37bcf7e16c211c75ec185eeba6f44c16502
yash2703 <60029684+yash2703@users.noreply.github.com>
2020-06-15 13:57:56 +0530

    final2

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ echo " Finally !!!!!!!!!";
 
 echo "Successfully completed  task 1 under guidance of Vimal Daga sir" ;
 
-echo "In this task I had integrated various tools  as shown in the image .."
-echo "hello"
+echo "In this task I had integrated various tools  as shown in the image ..";
+echo "hello";
 
 
 ?>
