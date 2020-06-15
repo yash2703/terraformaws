84dac48bb61da81da6ec22bc29bc270cd4be6d60
yash2703 <19ucs256@lnmiit.ac.in>
2020-06-15 15:14:21 +0530

    taskcompleted

--- index.php
+++ index.php
@@ -7,5 +7,8 @@ echo "Successfully completed  task 1 under guidance of Vimal Daga sir" ;
 echo "In this task I had integrated various tools  as shown in the image ..";
 echo "hello";
 
+echo "completed";
+
+
 
 ?>
